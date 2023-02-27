a1a31d146a121329851b2635a0cf49f9436a64b9
sycha11 <sycha112@naver.com>
2023-02-27 21:09:11 +0900

    2023 2/22 백준 10026(Gold5) dfs

--- seungyun/src/Siver3/Test11.java
+++ seungyun/src/Siver3/Test11.java
@@ -1,75 +1,81 @@
 package Siver3;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Scanner;
 
 public class Test11 {
-	
-	public static int[][] map;
-	
+	// 우하좌상
+	 	static int[] dx = {0,1,0,-1};
+	    static int[] dy = {1,0,-1,0};
+	    public static boolean[][] visited;
+	    public static int[][] map;
+	    public static int n;
 	public static void main(String[] args) {
-		int rows=6;
-		int columns=6;
-		int[][] queries;
-		
-		 int[] answer = {};
+		Scanner sc = new Scanner(System.in);
+		String[][] boards = {{"00111","00111","21001","11001","11111"},{"00000","00100","01210","00100","00000"}};
+		 int[] answer = new int[boards.length];
 	        
-	        map = new int[rows+1][columns+1];
-	        
-	        for(int i=1; i<=rows; i++){
-	            for(int j=1; j<=columns; j++){
-	                map[i][j] = (i-1) * columns  + j;
+	        int test_case = boards.length;
+	        for(int tc=0; tc<test_case; tc++){
+	            answer[tc] = 1;
+	            n = boards[tc].length;
+	            visited = new boolean[n][n];
+	            map = new int[n][n];
+
+	            for(int i=0; i<n; i++){
+	                for(int j=0; j<n; j++){
+	                    map[i][j] = boards[tc][i].charAt(j) - '0';
+	                    if(map[i][j] == 0){
+	                        visited[i][j] = true;
+	                    }
+	                }
 	            }
-	        }
-	        
-	        // for(int i=0; i<queries.length; i++){
-	        //     tona(queries[i]);
-	        // }
-	        
-	        int[] test = {2,2,5,4};
-	        tona(test);
-	        
-	         
-	        for(int i=1; i<=rows; i++){
-	            for(int j=1; j<=columns; j++){
-	                System.out.print(map[i][j]);
+
+	            for(int i=0; i<n; i++){
+	                for(int j=0; j<n; j++){
+	                    if(map[i][j] == 2){
+	                        dfs(i,j);
+	                    }
+	                }
+	            }
+
+	            for(int i=0; i<n; i++){
+	                for(int j=0; j<n; j++){
+	                   if(visited[i][j] == false){
+	                       answer[tc] = 0;
+	                   } 
+	                }
+	            }
+
+	            for(int i=0; i<n; i++){
+	                for(int j=0; j<n; j++){
+	                   System.out.print(visited[i][j]);
+	                }
+	                System.out.println("");
 	            }
 	            System.out.println("");
 	        }
-	        
+	       
 	}
-	 public static void tona(int[] qu){
-	        int a = qu[0];
-	        int b = qu[1];
-	        int c = qu[2];
-	        int d = qu[3];
-	        Queue<Integer> q = new LinkedList<>();
-	        
-	        q.add(map[a][b]);
-	        // i=a
-	        for(int j=b; j<d; j++){
-	            q.add(map[a][j+1]);
-	            map[a][j+1] = q.poll();
-	        }
-	        // j=d
-	        for(int i=a; i<c; i++){
-	            q.add(map[i+1][d]);
-	            map[i+1][d] = q.poll();
-	        }
-	        System.out.println(q.peek());
-	        // i=c
-	        for(int j=d; j>b; j--){
-	            q.add(map[c][j-1]);
-	            map[c][j-1] = q.poll();
-	        }
-	        System.out.println(map[5][4]);
-	        System.out.println(map[5][3]);
-	        System.out.println(map[5][2]);
-	        // j=b
-	        for(int i=c; i>a; i--){
-	            q.add(map[i-1][b]);
-	            map[i-1][b] = q.poll();
-	        }
-	        
-	    }
+	
+	public static void dfs(int a, int b){
+        visited[a][b] = true;
+        int cnt =0;
+        for(int k=0; k<4; k++){
+            int nextX = a + dx[k];
+            int nextY = b + dy[k];
+            if(nextX <0 || nextY <0 || nextX >= n || nextY >= n){
+            	cnt++;
+                continue;
+            }
+            if(visited[nextX][nextY]){
+                cnt++;
+                if(cnt == 4) {
+                	visited[a][b] = false;
+                }
+                continue;
+            }
+            System.out.println(a + " " + b);
+            dfs(nextX,nextY);
+        }
+    }
 }
