535a71aa30018d55864884f6a0558dd566876f87
sycha11 <sycha112@naver.com>
2023-03-27 20:04:20 +0900

    2023 3/27 2468(Siver1) dfs

---
+++ seungyun/src/Day20230327/Baek1747.java
@@ -0,0 +1,53 @@
+package Day20230327;
+
+import java.util.*;
+public class Baek1747 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int len = 10000001;
+		int[] A = new int[len];
+		
+		for(int i=2; i<len; i++) {
+			A[i] = i;
+		}
+		
+		for(int i=2; i<len; i++) {
+			if(A[i] == 0) {
+				continue;
+			}
+			
+			for(int j= i+i; j<len; j+=i) {
+				A[j] = 0;
+			}
+			
+		}
+		for(int i=n; i<len; i++) {
+			if(A[i] != 0) {
+				String str = Integer.toString(A[i]);
+				int le = str.length();
+				int cnt =0;
+				int left = 0;
+				int right = le-1;
+				while(left < right) {
+					if(str.charAt(left) == str.charAt(right)) {
+						left += 1;
+						right -= 1;
+						cnt++;
+					} else {
+						break;
+					}
+				}
+				if(cnt == le/2) {
+					System.out.println(A[i]);
+					break;
+				}
+			}
+		}
+		
+		
+	}
+
+}
