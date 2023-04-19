b45cf8b68e044833da529ba9c3b54ea881b4a3e2
sycha11 <sycha112@naver.com>
2023-04-19 19:57:04 +0900

    2023 04/19 Baek1987(Gold4) dfs

--- seungyun/src/Day_20230414/ExGitTest.java
+++ seungyun/src/Day_20230414/ExGitTest.java
@@ -6,6 +6,17 @@ public class ExGitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(gcd(9,21));
+		
+		int i=0;
+		int sum=0;
+		while(i<10) {
+			i++;
+			if(i%2==1)
+				continue;
+			sum += i;
+			System.out.println(i);
+		}
+		System.out.println(sum);
 	}
 	
 	public static int gcd(int a, int b) {
